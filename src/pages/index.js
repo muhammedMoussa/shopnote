269e52cb2cd057e6b35c68e038c9cf57cb3f0547
muhammedMoussa <muhammedmoussa@hotmail.com>
2020-12-12 01:04:06 +0200

    config gatsby

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';    
+ 
+export default () => {    
+  const [loading, setLoading ] = useState(false);    
+  const [shopnotes, setShopnotes] = useState(null);    
+ 
+  return (
+    <>    
+      <h1>Shopnotes to load here...</h1>
+    </>        
+  )    
+}
